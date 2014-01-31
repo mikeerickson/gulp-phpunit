b18f5edfe53ad2a809c05bf899d42bb8b430a7d7
Mike Erickson <codedungeon@gmail.com>
2014-01-30 17:56:47 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+/*jshint node:true */
+
+"use strict";
+
+var map = require('map-stream'),
+	gutil = require('gulp-util'),
+	 exec = require('child_process').exec;
+
+module.exports = function(command, opt){
+	if (!command) {
+		throw new Error('command is blank');
+	}
+	// defaults
+	if (!opt) {
+		opt = {};
+	}
+	if (typeof opt.silent === 'undefined') {
+		opt.silent = false;
+	}
+
+	return map(function (file, cb){
+		var cmd = gutil.template(command, {file: file, options: opt});
+
+		console.log(cmd);
+
+		exec(cmd, function (error, stdout, stderr) {
+			if (!opt.silent && stderr) {
+				gutil.log(stderr);
+			}
+			if (stdout) {
+				stdout = stdout.trim(); // Trim trailing cr-lf
+			}
+			if (!opt.silent && stdout) {
+				gutil.log(stdout);
+			}
+			cb(error, file);
+		});
+	});
+};
